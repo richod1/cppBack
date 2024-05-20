346e2db95c9fa4ba293298e8ab74920ce1a7f063
Degraft Frimpong <frimpsup@gmail.com>
2024-05-20 11:47:12 -0400

    change watch added

--- httpServer/httpServer.h
+++ httpServer/httpServer.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include <map>
 #include <netinet/in.h>
+#include <thread>
+#include <sys/wait.h>
 
 
 class HttpServer{
@@ -21,9 +23,40 @@ public:
         getRoutes[path]=handler;
 
     }
+
     void post(const std::string& path,Handler handler){
         postRoutes[path]=handler;
 
+    }
+    // method to start server
+    void start(){
+        server_thread=std::thread(&HttpServer::run_server,this);
+        server_thread.join();
+        
+    }
+
+
+    void watch_and_reload(const std::string &source_file){
+        std::thread([this,source_file](){
+            while(true){
+                std::string command="fswatch -1" + source_file;
+                system(command.c_str());
+                std::cout<<"Change detected in " + source_file + ". Recompiling and restarting server..."<<std::endl;
+
+                if(server_thread.joinable()){
+                    server_thread.detach();
+                }
+
+                // recompile the server
+
+                std::string compile_command= "g++ -o http_server " + source_file +" -std=c++13";
+                system(compile_command.c_str());
+
+                // Restart the server
+                server_thread=std::thread(&HttpServer::run_server,this);
+            }
+
+        }).detach();
     }
     // server listner
     void listen(){
@@ -100,6 +133,79 @@ public:
         std::vector<Handler> middlewares;
         std::map<std::string, Handler>  getRoutes;
         std::map<std::string, Handler> postRoutes;
+        std::thread server_thread;
+
+        void run_server(){
+            int server_fd,new_socket;
+            struct sockaddr_in address;
+            int opt=1;
+            int addrlen=sizeof(address);
+
+            if((server_fd=socket(AF_INET,SOCK_STREAM,0))==0){
+                perror("socket failed");
+                exit(EXIT_FAILURE);
+            }
+
+            if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))){
+            perror("setsockopt");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family=AF_INET;
+        address.sin_addr.s_addr=INADDR_ANY;
+        address.sin_port=htons(port);
+
+
+        // checking binding
+          if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+            perror("bind failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (::listen(server_fd, 3) < 0) {
+            perror("listen");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout<<"Server is listening port :"<<port<<std::endl;
+
+
+    while(true){
+        if((new_socket=accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen)) < 0){
+            perror("accept");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+    
+
+    std::string request=read_request(new_socket);
+    std::map<std::string,std::string> headers=parse_headers(request);
+    std::string method,path;
+    parse_request_line(request,method,path);
+    std::string body=get_request_body(request);
+
+
+    for(const auto &middleware:middlewares){
+        middleware(new_socket,headers,body);
+    }
+
+    if(method == "GET" && getRoutes.find(path) != getRoutes.end()){
+        getRoutes[path](new_socket,headers,body);
+    }else if(method=="POST" && postRoutes.find(path) != postRoutes.end()){
+        postRoutes[path](new_socket,headers,body);
+    }else{
+        std::string response ="HTTP/1.1 404 NOT FOUND \nContent-Type:text/plain\nContent-Length: 9\n\nNot Found";
+        send(new_socket,response.c_str(),response.length(),0);
+        close(new_socket);
+        }
+    }
+
+    close(server_fd);
+
+
+        }
 
 
         // function for reading request
--- main.cpp
+++ main.cpp
@@ -1,68 +1,44 @@
+// Example usage of httpServer
 #include <iostream>
 #include <string>
+#include <map>
 #include <cstring>
-#include <sstream>
-#include <unistd.h>
-#include <netinet/in.h>
-#include "HTTP/handle_request.h"
-#include "HTTP/parse_request.h"
-#include "HTTP/read_request.h"
+#include "httpServer/httpServer.h"
 
 #define PORT 8080
+
 int main(){
+    HttpServer server(PORT);
 
-    int server_fd,new_socket;
-
-    struct sockaddr_in address;
-
-    int opt=1;
-    int addrlen=sizeof(address);
-
-    if((server_fd=socket(AF_INET,SOCK_STREAM,0))==0){
-        perror("sockt failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,&opt,sizeof(opt))){
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-
-    address.sin_family=AF_INET;
-    address.sin_addr.s_addr=INADDR_ANY;
-    address.sin_port=htons(PORT);
-
-    // binding port
-    if(bind(server_fd,(struct sockaddr *)&address,sizeof(address))<0){
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-    // listen to poat
-    if(listen(server_fd,3)<0){
-        perror("listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-
-    }
-
-    std::cout<<"Server listening on port "<<PORT<<std::endl;
-
-
-    while(true){
-        if(new_socket=accept(server_fd,(struct sockaddr *)&address,(socklen_t*)&addrlen)){
-            perror("accept");
-            close(server_fd);
-            exit(EXIT_FAILURE);
+    // middlware for loging
+    server.use([](int socket,const std::map<std::string,std::string>& headers,const std::string& body){
+        std::cout<<"Middleware: Received request with headers: \n";
+        for(const auto& header:headers){
+            std::cout<<header.first<<": "<<header.second<<"\n";
         }
+    });
 
-        std::string request=read_request(new_socket);
-        std::string path=parse_request(request);
-        handle_request(new_socket,path);
-    }
+    // defining the get route
+    server.get("/",[](int socket,const std::map<std::string,std::string>& headers,const std::string& body){
+        std::string response="HTTP/1.1 200 OK \nContent-Type: text/plain\nContent-Length: 12\n\nHello World";
+        send(socket,response.c_str(),response.length(),0);
+        close(socket);
+    });
 
-    close(server_fd);
+    // having a specific route
+    server.get("/api",[](int socket,const std::map<std::string,std::string>& headers,const std::string& body){
+        std::string response="HTTP/1.1 200 OK \nContent-Type: text/plain\nContent-Length: 5\n\nHello Api";
+        send(socket,response.c_str(),response.length(),0);
+        close(socket);
+    });
+
+    server.post("/data",[](int socket,const std::map<std::string,std::string>& headers,const std::string& body){
+        std::string response="HTTP/1.1 200 OK\nContent-Type: application/json\nContent-Length: "+ std::to_string(body.length()) + "\n\n" + body;
+        send(socket,response.c_str(),response.length(),0);
+        close(socket);
+    });
+
+    server.start();
+
     return 0;
-
 }
