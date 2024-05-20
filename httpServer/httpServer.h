13744008647f64d02c2c083370e45b48089e2db1
Degraft Frimpong <degraft@degraft.degraft>
2024-05-20 08:27:32 -0400

    req binding added

--- httpServer/httpServer.h
+++ httpServer/httpServer.h
@@ -25,6 +25,43 @@ public:
         postRoutes[path]=handler;
 
     }
+    // server listner
+    void listen(){
+        int server_fd,new_socket;
+        struct sockaddr_in address;
+        int opt=1;
+        int addrlen=sizeof(address);
+
+        if((server_fd=socket(AF_INET,SOCK_STREAM,0))==0){
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+        if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))){
+            perror("setsockopt");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family=AF_INET;
+        address.sin_addr.s_addr=INADDR_ANY;
+        address.sin_port=htons(port);
+
+
+        // checking binding
+          if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+            perror("bind failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (::listen(server_fd, 3) < 0) {
+            perror("listen");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout<<"Server is listening port :"<<port<<std::endl;
+
+    }
 
     private:
         int port;
